fe47d2c2b9cce122c1bfce9fba60861fb5478277
novelshort <yq963657293@outlook.com>
2025-01-02 18:47:48 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -245,7 +245,7 @@ router.post('/webhook', async (ctx) => {
     const event = stripe.webhooks.constructEvent(
       ctx.request.rawBody, // 原始的请求体，确保使用了中间件保留原始 body
       sig,
-      process.env.WEBHOOK_SECRET
+      'whsec_83f7a5ca49aa677264d6f03df4c06d0db31cda0144db5f62e79a7965466a6f99'
     );
 
     if (event.type === 'payment_intent.succeeded') {//payment_intent.succeeded 是支付成功的 Webhook 事件类型
